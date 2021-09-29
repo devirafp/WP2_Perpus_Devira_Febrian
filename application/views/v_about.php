9325a846dc97d23b88c2d2a327438eef0bcc3001
devirafp <devirapratiwi2@gmil.com>
2021-09-29 14:30:55 +0700

    initial commit

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Devira</li>
+        <li>Nama Belakang : Febrian</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Kp.Curug </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bogor</li>
+    </ul>
+    <h4>Makanan Favorit</h4>
+    <ul type="square">
+        <li>Nasi Goreng</li>
+        <li>Mie Ayam </li>
+    </ul>
+</section>
