9ffc5166550f5a19067d74a2ef006439197a5e33
devirafp <devirapratiwi2@gmil.com>
2021-10-05 14:57:25 +0700

    pesan comit

---
+++ application/controllers/MataKuliah.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') or exit('Rejected');
+class MataKuliah extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->helper('url');
+    }
+    public function index()
+    {
+        $this->load->view('view-form-matakuliah');
+    }
+    public function cetak()
+    {
+
+        $this->form_validation->set_rules(
+            'kode',
+            'Kode Matakuliah',
+            'required|min_length[3]',
+            [
+                'required' => 'Kode Matakuliah Harus diisi',
+                'min_lenght' => 'Kode terlalu pendek'
+            ]
+        );
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama Matakuliah',
+            'required|min_length[3]',
+            [
+                'required' => 'Nama Matakuliah Harus diisi',
+                'min_lenght' => 'Nama terlalu pendek'
+            ]
+        );
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+            $this->load->view('view-data-matakuliah', $data);
+        }
+    }
+}
